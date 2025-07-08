4c0bab6d697fb9af829e4922c0ccd241461a8637
JT Archie <jtarchie@gmail.com>
2025-07-07 22:13:32 -0600

    Initial Commit

---
+++ ActivityExport/ActivityExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityExportApp.swift
+//  ActivityExport
+//
+//  Created by JT Archie on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ActivityExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivityExportTests/ActivityExportTests.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityExportTests.swift
+//  ActivityExportTests
+//
+//  Created by JT Archie on 7/7/25.
+//
+
+import Testing
+@testable import ActivityExport
+
+struct ActivityExportTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
